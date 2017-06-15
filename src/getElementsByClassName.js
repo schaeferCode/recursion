2940552f9b68ad36d587b90e7cbff14809b4ec42
Scott Schaefer <scottschaef@gmail.com>
2017-06-15 16:27:53 -0400

    Include all HTML, not just body in the function
    I switch the two conditional statements to make the function
    pass all the tests.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,21 +5,22 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
-  var doc = document.body; 
   var result = []; 
 
-  function findClass (node) { 
-    for (var i = 0; i < node.children.length; i++) {
-      if (node.children[i].children.length > 0) {
-        findClass (node.children[i]);
-      }
+  function findClass (node) {
 
+
+    for (var i = 0; i < node.children.length; i++) {
       if (node.children[i].classList.length > 0 && node.children[i].classList.value.indexOf(className) >= 0) {
         result.push(node.children[i])
       }
+
+      if (node.children[i].children.length > 0) {
+        findClass (node.children[i]);
+      }       
     }
   }
 
-  findClass(doc);
+  findClass(document);
   return result;
 }

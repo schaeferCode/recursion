907034fc491b5968cea5fb30221be0e39db94916
Scott Schaefer <scottschaef@gmail.com>
2017-06-15 14:34:54 -0400

    Create a working structure

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
-};
+var getElementsByClassName = function(className) {
+  var doc = document.body; 
+  var result = []; 
+
+  function findClass (node) { 
+    for (var i = 0; i < node.children.length; i++) {
+      if (node.children[i].children.length > 0) {
+        findClass (node.children[i]);
+      }
+
+      if (node.children[i].classList.length > 0 && node.children[i].classList.value.indexOf(className) >= 0) {
+        result.push(node.children[i])
+      }
+    }
+  }
+
+  findClass(doc);
+  return result;
+}
